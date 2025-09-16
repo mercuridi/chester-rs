59cb2821932f71a6db3a94209214095d80ed7cb6
mercuridi <kaibarberharris@protonmail.com>
2025-09-16 18:10:17 +0100

    add dotenv reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use poise::serenity_prelude as serenity;
 
+use dotenv::dotenv;
+
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
@@ -18,6 +20,7 @@ async fn age(
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
     let intents = serenity::GatewayIntents::non_privileged();
 
