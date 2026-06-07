27cf7a414c98df2c9b116ea10e1dee219cc75f6b
mercuridi <kaibarberharris@protonmail.com>
2026-06-07 18:16:42 +0100

    fix: library incomplete function displaying incorrect data fixed

--- src/db/repository.rs
+++ src/db/repository.rs
@@ -159,7 +159,7 @@ pub async fn fetch_library_by_incomplete(db_pool: &SqlitePool) -> Result<Vec<Vec
             LEFT JOIN origins ON tracks.origin_id = origins.id
             WHERE artists.artist = 'No artist provided'
             OR origins.origin = 'No origin provided'
-            ORDER BY tracks.track_title"
+            ORDER BY artists.artist, origins.origin, tracks.track_title"
     )
     .fetch_all(db_pool)
     .await
@@ -167,8 +167,8 @@ pub async fn fetch_library_by_incomplete(db_pool: &SqlitePool) -> Result<Vec<Vec
 
     Ok(rows.into_iter().map(|row| vec![
         row.try_get::<String, _>(0).unwrap_or_else(|_| "No title".to_string()),
-        row.try_get::<String, _>(0).unwrap_or_else(|_| "No artist".to_string()),
-        row.try_get::<String, _>(1).unwrap_or_else(|_| "No origin".to_string()),
+        row.try_get::<String, _>(1).unwrap_or_else(|_| "No artist".to_string()),
+        row.try_get::<String, _>(2).unwrap_or_else(|_| "No origin".to_string()),
     ]).collect())
 }
 
