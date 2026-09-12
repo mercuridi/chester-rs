398730f6ce4137afd0ae9ba0b9e113df20cb1013
mercuridi <kaibarberharris@protonmail.com>
2026-09-12 16:59:34 +0100

    refactor: mod.rs removal and import updates: src/discord/

---
+++ src/discord.rs
@@ -0,0 +1,5 @@
+mod autocomplete;
+pub(crate) mod commands;
+pub(crate) mod constants;
+pub(crate) mod context;
+mod voice;
--- src/discord/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod autocomplete;
-pub mod commands;
-pub mod constants;
-pub mod context;
-pub mod voice;
